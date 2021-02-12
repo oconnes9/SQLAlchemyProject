eb767a7493d60a4f26e9ad3a5ff78e37e9ac0e9b
seanoconnell <oconnes9@tcd.ie>
2021-02-12 14:56:08 +0000

    create_tables.py updated and working.

--- code1/create_tables.py
+++ code1/create_tables.py
@@ -6,9 +6,13 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
+create_table = "CREATE TABLE IF NOT EXISTS stores (id INTEGER PRIMARY KEY, name text)"
+cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real, store_id INTEGER, FOREIGN KEY(store_id) REFERENCES stores(id))"
 cursor.execute(create_table)
 
 connection.commit()
 
 connection.close()
+
